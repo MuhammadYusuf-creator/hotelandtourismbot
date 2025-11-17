9dce8f641a326cae18973a80f87fedb20630c1e1
MuhammadYusuf-creator <mukhammadyusuf106@gmail.com>
2025-11-17 15:54:30 +0500

    intro finishedas

--- src/main/java/org/example/Hotelservice.java
+++ src/main/java/org/example/Hotelservice.java
@@ -1,6 +1,42 @@
 package org.example;
 
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hotelservice {
 
+    public static SendMessage intro(Long chatId) {
+        SendMessage SM = new SendMessage();
+        SM.setChatId(chatId);
+        SM.setText("Assalomu alaykum Iltimos tilni tanlang :");
 
+        InlineKeyboardMarkup imk = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowlist = new ArrayList<>();
+
+        List<InlineKeyboardButton> row = new ArrayList<>();
+        InlineKeyboardButton btn = new InlineKeyboardButton();
+        btn.setText("🇺🇿O'zbekcha");
+        btn.setCallbackData("ozbekId");
+        row.add(btn);
+        rowlist.add(row);
+
+        InlineKeyboardButton button = new InlineKeyboardButton();
+        button.setText("🇷🇺Russian");
+        button.setCallbackData("rusId");
+        row.add(button);
+
+        InlineKeyboardButton button1 = new InlineKeyboardButton();
+        button1.setText("🇺🇸English");
+        button1.setCallbackData("englishId");
+        row.add(button1);
+
+
+        imk.setKeyboard(rowlist);
+        SM.setReplyMarkup(imk);
+        return SM;
+    }
 }
--- src/main/java/org/example/myBot.java
+++ src/main/java/org/example/myBot.java
@@ -2,12 +2,29 @@ package org.example;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class myBot extends TelegramLongPollingBot {
 
+    Hotelservice hotel = new Hotelservice();
+    Tourismservice tourism = new Tourismservice();
+
     @Override
     public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String text = update.getMessage().getText();
+            Long chatId = update.getMessage().getChatId();
 
+
+            if (text.equals("/start")) {
+                try {
+                    execute(hotel.intro(chatId));
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+        }
     }
 
 
