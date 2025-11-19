c194fb07b36bfb7ef84b261ddfbb90e589a4d35e
MuhammadYusuf-creator <mukhammadyusuf106@gmail.com>
2025-11-19 09:06:14 +0500

    shaharlar tugadi

--- src/main/java/org/example/Hotelservice.java
+++ src/main/java/org/example/Hotelservice.java
@@ -86,7 +86,69 @@ public class Hotelservice {
         row.add(button);
         rowlist.add(row);
 
+
         List<InlineKeyboardButton> row1 = new ArrayList<>();
+        InlineKeyboardButton btn1 = new InlineKeyboardButton();
+        btn1.setText("Buxoro");
+        btn1.setCallbackData("buxoroId");
+        row1.add(btn1);
+
+        InlineKeyboardButton btn2 = new InlineKeyboardButton();
+        btn2.setText("Jizzax");
+        btn2.setCallbackData("jizzaxId");
+        row1.add(btn2);
+        rowlist.add(row1);
+
+
+        List<InlineKeyboardButton> row3 = new ArrayList<>();
+        InlineKeyboardButton btn3 = new InlineKeyboardButton();
+        btn3.setText("Qashqadaryo");
+        btn3.setCallbackData("qashqadaryoId");
+        row3.add(btn3);
+
+        InlineKeyboardButton btn4 = new InlineKeyboardButton();
+        btn4.setText("Navoiy");
+        btn4.setCallbackData("navoiyId");
+        row3.add(btn4);
+        rowlist.add(row3);
+
+
+        List<InlineKeyboardButton> row4 = new ArrayList<>();
+        InlineKeyboardButton btn5 = new InlineKeyboardButton();
+        btn5.setText("Namangan");
+        btn5.setCallbackData("namanganId");
+        row4.add(btn5);
+
+        InlineKeyboardButton btn6 = new InlineKeyboardButton();
+        btn6.setText("Samarqand");
+        btn6.setCallbackData("SamarqandId");
+        row4.add(btn6);
+        rowlist.add(row4);
+
+        List<InlineKeyboardButton> row5 = new ArrayList<>();
+        InlineKeyboardButton btn7 = new InlineKeyboardButton();
+        btn7.setText("Surxondaryo");
+        btn7.setCallbackData("surxondaryoId");
+        row5.add(btn7);
+
+        InlineKeyboardButton btn8 = new InlineKeyboardButton();
+        btn8.setText("Sirdaryo");
+        btn8.setCallbackData("sirdaryoId");
+        row5.add(btn8);
+        rowlist.add(row5);
+
+
+        List<InlineKeyboardButton> row6 = new ArrayList<>();
+        InlineKeyboardButton btn9 = new InlineKeyboardButton();
+        btn9.setText("Farg'ona");
+        btn9.setCallbackData("fargonaId");
+        row6.add(btn9);
+
+        InlineKeyboardButton btn10 = new InlineKeyboardButton();
+        btn10.setText("Xorazm");
+        btn10.setCallbackData("xorazmId");
+        row6.add(btn10);
+        rowlist.add(row6);
 
 
         imk.setKeyboard(rowlist);
